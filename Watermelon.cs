5e247699c9d3970583a272cd2fcbe7576f069d45
Rezwan-Huq <59844044+Rezwan-Huq@users.noreply.github.com>
2020-01-14 00:57:09 +0600

    Watermelon

---
+++ Watermelon.cs
@@ -0,0 +1,20 @@
+using System; 
+  
+class GFG { 
+    // Main Method 
+    static public void Main() 
+    { 
+    	int x;
+    	x = int.Parse(Console.ReadLine());
+    	if (x%2!=0 || x==2)
+
+    	{
+    		Console.WriteLine("NO");
+
+    	}
+    	else {
+
+    		Console.WriteLine("YES");
+    	}
+    } 
+} 
